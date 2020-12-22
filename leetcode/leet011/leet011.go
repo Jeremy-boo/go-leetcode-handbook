3e05d781ceee4d668086bbd2dc3f8b90ddb43f18
Jeremy-boo <zb_9507@outlook.com>
2020-12-22 14:15:37 +0800

    feature：翻转二叉树

---
+++ leetcode/leet011/leet011.go
@@ -0,0 +1,21 @@
+package leet011
+
+// TreeNode Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	// 递归终止条件
+	if root == nil {
+		return nil
+	}
+	// 处理当前逻辑，右子树变成左子树
+	root.Left, root.Right = root.Right, root.Left
+	// 下探到下一层
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
