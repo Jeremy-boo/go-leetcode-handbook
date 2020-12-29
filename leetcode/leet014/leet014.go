d5e1a308b25765aa18d21e2640f501af7db6ba32
Jeremy-boo <boo-jeremy@outlook.com>
2020-12-29 22:58:12 +0800

    add: 子集

--- leetcode/leet014/leet014.go
+++ leetcode/leet014/leet014.go
@@ -1,17 +1,21 @@
 package leet014
 
+import "fmt"
+
+// subsets 子集
 func subsets(nums []int) [][]int {
-	res := make([][]int, 0)
-	mid := make([]int, 0)
-	dfs(0, nums, &mid, &res)
-	return res
+	ans := make([][]int, 0)
+	set := make([]int, 0)
+	dfs(0, nums, &set, &ans)
+	return ans
 }
 
 // 递归来做
 func dfs(cur int, nums []int, mid *[]int, res *[][]int) {
 	// 终止条件
 	if cur == len(nums) {
-		*res = append(*res, *mid)
+		fmt.Println(append([]int(nil), *mid...))
+		*res = append(*res, append([]int(nil), *mid...))
 		return
 	}
 	// 处理当前逻辑 每一个节点都有两个分支，一个选一个不选
