186637b33451096bcb3bed19113deb388e7af584
Jeremy-boo <zb_9507@outlook.com>
2021-03-23 11:48:23 +0800

    feat:移除链表元素，环形链表，链表的中间节点

--- leetcode/斐波那契数列/leet.go
+++ leetcode/斐波那契数列/leet.go
@@ -1,10 +1,17 @@
-package 斐波那契数列
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fib1(6))
+}
 
 // 使用递归的方法 肯定超时
 func fib(n int) int {
 	if n <= 1 {
 		return n
 	}
+
 	return fib(n-1) + fib(n-2)
 }
 
@@ -13,12 +20,12 @@ func fib1(n int) int {
 	if n == 0 || n == 1 {
 		return n
 	}
-	f0, f1, f2 := 0, 1, 1
-	for i := 2; i <= n; i++ {
+	f1, f2, f3 := 1, 1, 2
+	for i := 2; i < n; i++ {
 		// 根据题意，当N足够大的时候 int32 或者 int64会发生越界
-		f2 = (f1 + f0) % 1000000007
-		f0 = f1
+		f3 = (f2 + f1) % 1000000007
 		f1 = f2
+		f2 = f3
 	}
-	return f2
+	return f3
 }
